a88f50c4f09905932b73c4a4906750ea0e1cec84
John Kenneth Rosales <89952648+JKDRosales11@users.noreply.github.com>
2022-05-30 14:43:45 +0800

    remove searchbar

--- resources/views/support.blade.php
+++ resources/views/support.blade.php
@@ -34,16 +34,6 @@
                     <div class="container">
                         <h1 class="col text-center">Help Center</h1>
                         <div class="row d-flex justify-content-center">
-                            <div class="col-md-10 col-lg-8 col-xl-7 mx-auto ">
-                                <div class="p-2">
-                                    <div class="input-group input-group-lg rounded border border-dark">
-                                        <input type="text" class="form-control form-control-lg rounded" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2" />
-                                        <span class="input-group-text" id="basic-addon2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                                            </svg></span>
-                                    </div>
-                                </div>
-                            </div>
                             <div class="mt-3"> <br>
                                 <p class="opacity-50 text-center"> Get started in using our website through our help center. If you need <br> any help, feel free to contact our support team.</p>
                             </div>
